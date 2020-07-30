71ac95ca3e9df79c00af0ba302a0a0b209ea4902
SteveMcCoy <steve_mccoy@hotmail.co.uk>
2020-07-30 19:04:23 +0100

    stuff

--- python/life.py
+++ python/life.py
@@ -1,13 +1,13 @@
 import numpy as np
 import time
-from graphics import GraphWin, Line, Point, Rectangle
+from graphics import GraphWin, Line, Point, Rectangle, color_rgb
 
 class Life:
 
     # Background
-    bgcolour = 'black'
+    bgcolour = 'gray'
     # For grid lines
-    gridcolour = 'white'
+    gridcolour = color_rgb(200,200,200)
     # For live cells
     cellcolour = 'blue'
 
@@ -43,7 +43,7 @@ class Life:
             for y in range(self.ny):
                 self.aframe[x][y] = self.bframe[x][y]
     
-    # Load an initial figure into the array, at the centre.
+   # Load an initial figure into the array, at the centre.
     def load(self, initial):
         (w,h) = np.shape(initial)
         startx = int((self.nx - w)/2)
@@ -68,11 +68,11 @@ class Life:
     def is_cell_live(self, x, y):
         if x < 0:
             x += self.nx
-        if x >= self.nx:
+        elif x >= self.nx:
             x %= self.nx
         if y < 0:
             y += self.ny
-        if y >= self.ny:
+        elif y >= self.ny:
             y %= self.ny
         return self.aframe[x][y] != 0
                         
@@ -88,7 +88,7 @@ class Life:
         return count
     
     def create_window(self):
-        initial_size = 500
+        initial_size = 800
         self.win = GraphWin("Conway's Game of Life", initial_size, initial_size)
         self.win.setBackground(self.bgcolour)
         return self.win
@@ -128,8 +128,9 @@ class Life:
                 self.draw_cell(x, y, self.aframe[x][y])
                 
     def update_cells(self):
-        for y in range(self.ny):
-            for x in range(self.nx):
+        lowx, lowy, highx, highy = self.bounding_box_plus()
+        for y in range(lowy, highy):
+            for x in range(lowx, highx):
                 count = self.neighbour_count(x, y)
                 state = self.is_cell_live(x, y)
                 new_state = state
@@ -140,9 +141,52 @@ class Life:
                     new_state = 0
                     self.draw_cell(x, y, 0)
                 self.bframe[x][y] = new_state
+                
+    def bounding_box_plus(self):
+ #       return 0, 0, self.nx, self.ny
+        cmin, rmin, cmax, rmax = bounding_box(self.aframe)
+        cmin -= 1
+        rmin -= 1
+        cmax += 1
+        rmax += 1
+        if cmin < 0 or cmax >= self.nx:
+            cmin, cmax = 0, self.nx
+        if rmin < 0 or rmax >= self.ny:
+            rmin, rmax = 0, self.ny
+        print(cmin, rmin, cmax, rmax)
+        return cmin, rmin, cmax, rmax
+    
+    def draw_bb(self, lowx, lowy, highx, highy):
+        (gx, gy) = self.cell_coord(lowx, lowy)
+        (gx2, gy2) = self.cell_coord(highx, highy)
+        box = Rectangle(Point(gx, gy), Point(gx2, gy2))
+        box.set
+
+        # Draw given cell in the given state.
+    def draw_cell(self, x, y, state):
+        (gx, gy) = self.cell_coord(x, y)
+        inset = 1
+        box = Rectangle(
+            Point(gx + inset, gy + inset), 
+            Point(gx + self.cell_width - inset, gy + self.cell_height - inset))
+        box.setFill(self.bgcolour if state == 0 else self.cellcolour)
+        box.draw(self.win)
+        
+
+def bounding_box(img):
+    anycell = np.any(img)
+    rows = np.any(img, axis = 1)
+    cols = np.any(img, axis = 0)
+    if anycell:
+        rmin, rmax = np.where(rows)[0][[0,-1]]
+        cmin, cmax = np.where(cols)[0][[0,-1]]
+    else:
+        rmin, rmax = 0, 0
+        cmin, cmax = 0, 0
+    return cmin, rmin, cmax, rmax
         
 def main():
-    life = Life(30, 30)
+    life = Life(50, 50)
     life.draw_grid()
     initialConfig = np.array([[1,1,1],[0,0,1],[0,1,0]])
     life.load(initialConfig)
@@ -152,22 +196,22 @@ def main():
     while (True):
         life.update_cells()
         life.refresh_frames()
+        if life.win.checkMouse() != None:
+            break
         time.sleep(0.1)
-        
     
-    life.win.getMouse()
     life.win.close()
 
 main()
 
+# Need to start up the graphics window and provide an update loop which does 
+# the game of life step by step.
+
+# Make sure that the program quits when an exception occurs.
+
+# Provide buttons to control the program and allow for these to be handled.
+
+# 
 
 
-# game = Life(20,20)
-# game.load(initialConfig)
-# game.refresh_frames()
-# game.display()
-
-
-
-
-
+###
